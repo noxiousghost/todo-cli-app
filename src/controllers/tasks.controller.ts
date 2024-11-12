57756ee257386d4f3f848f04de1c1d7269d370c8
noxiousghost <sapkota.king@gmail.com>
2024-11-12 15:45:41 +0545

    add controller logic for creating a new task

---
+++ src/controllers/tasks.controller.ts
@@ -0,0 +1,27 @@
+import { Task, TaskStatus } from '../interface/task.interface';
+import { v4 as uuidv4 } from 'uuid';
+import { filePath } from '../utils/checkFile';
+import { writeJsonFile, readJsonFile } from '../utils/jsonOperations';
+
+export const createTask = async (options: Omit<Task, 'status' | 'isArchived'>): Promise<void> => {
+  const newTask: Task = {
+    id: uuidv4(),
+    name: options.name,
+    deadline: new Date(options.deadline),
+    status: TaskStatus.TODO,
+    tags: options.tags || [],
+    isArchived: false,
+  };
+  let tasks = [];
+  try {
+    const readJsonRes = await readJsonFile(filePath);
+    tasks = readJsonRes;
+    tasks.push(newTask);
+
+    await writeJsonFile(filePath, tasks);
+    console.log('New Task Added: ');
+    console.log(newTask);
+  } catch (error) {
+    console.error(error);
+  }
+};
