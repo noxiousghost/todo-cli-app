20209d3a87dfd101bd5ef3e33aa958509395f705
noxiousghost <sapkota.king@gmail.com>
2024-11-12 15:45:00 +0545

    add methods for reading from a json file and writing into json file

---
+++ src/utils/jsonOperations.ts
@@ -0,0 +1,26 @@
+import util from 'util';
+import fs from 'fs';
+import { Task } from '../interface/task.interface';
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
+export const readJsonFile = async (filePath: string): Promise<Task[]> => {
+  if (!fs.existsSync(filePath)) {
+    throw new Error('JSON file not found');
+  }
+  const data = await readFile(filePath, 'utf-8');
+  // checking if the json file contains any invalid syntax
+  try {
+    JSON.parse(data);
+  } catch (_err) {
+    throw new Error('JSON file contains invalid syntax. Fix it manually');
+  }
+  return data ? JSON.parse(data) : [];
+};
+
+export const writeJsonFile = async (filePath: string, allTasks: Task[]): Promise<void> => {
+  if (!fs.existsSync(filePath)) {
+    throw new Error('JSON file not found');
+  }
+  await writeFile(filePath, JSON.stringify(allTasks, null, 2), 'utf-8');
+};
