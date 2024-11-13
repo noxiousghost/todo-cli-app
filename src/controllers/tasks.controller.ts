ec383c398778700428052090cf3bccdcfbc2adbe
noxiousghost <sapkota.king@gmail.com>
2024-11-13 15:19:06 +0545

    add validation for tasks and use zod schema as type checking where possible.

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,20 +1,28 @@
-import { Task, TaskStatus } from '../interface/task.type';
+import { z } from 'zod';
+import { TaskStatus, taskSchema } from '../model/zod.schema';
 import { v4 as uuidv4 } from 'uuid';
 import { filePath } from '../utils/checkFile';
 import { writeJsonFile, readJsonFile } from '../utils/jsonOperations';
 
-export const createTask = async (options: Omit<Task, 'status' | 'isArchived'>): Promise<void> => {
-  const newTask: Task = {
-    id: uuidv4(),
-    name: options.name,
-    deadline: new Date(options.deadline),
-    status: TaskStatus.TODO,
-    tags: options.tags || [],
-    isArchived: false,
-  };
-  let tasks = [];
+export const createTask = async (options: z.infer<Omit<typeof taskSchema, 'status' | 'isArchived'>>): Promise<void> => {
   try {
+    const newTask: z.infer<typeof taskSchema> = {
+      id: uuidv4(),
+      name: options.name,
+      deadline: new Date(options.deadline),
+      status: TaskStatus.TODO,
+      tags: options.tags,
+      isArchived: false,
+    };
+    let tasks = [];
+
+    const parsedTask = taskSchema.safeParse(newTask);
+    if (!parsedTask.success) {
+      console.error('Validation failed:', parsedTask.error.flatten());
+      return;
+    }
     const readJsonRes = await readJsonFile(filePath);
+    console.log(typeof readJsonRes);
     tasks = readJsonRes;
     tasks.push(newTask);
 
