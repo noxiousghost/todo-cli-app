259e7fec7fa72b487240802e809070fb3bf1675d
noxiousghost <sapkota.king@gmail.com>
2024-11-14 11:49:00 +0545

    remove unused commented lines

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -39,18 +39,6 @@ export default [
           endOfLine: 'auto',
         },
       ],
-      // 'no-unused-vars': [
-      //   'error',
-      //   {
-      //     args: 'all',
-      //     argsIgnorePattern: '^_',
-      //     caughtErrors: 'all',
-      //     caughtErrorsIgnorePattern: '^_',
-      //     destructuredArrayIgnorePattern: '^_',
-      //     varsIgnorePattern: '^_',
-      //     ignoreRestSiblings: true,
-      //   },
-      // ],
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
