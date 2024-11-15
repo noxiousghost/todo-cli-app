c0c433bb616d86f29206d088b22aba76e83c1b1e
noxiousghost <sapkota.king@gmail.com>
2024-11-15 13:23:09 +0545

    wrap everything in async function and initialize dirPath and filePath vars

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import { Command } from 'commander';
 import { createTask } from '@src/controllers/tasks.controller';
-import { checkFilePath } from '@src/utils/checkFile';
+import { handleStorageExist } from '@src/utils/checkFile';
+import path from 'path';
 
-const program = new Command();
-checkFilePath();
+export const dirPath = path.resolve(__dirname, './jsons/');
+export const filePath = path.join(dirPath, '/tasks.json');
 
-program
-  .name('todo-cli')
-  .description('Manage your most important tasks from the comfort of your terminal')
-  .version('1.0.0');
+const run = async (): Promise<void> => {
+  const program = new Command();
+  await handleStorageExist(dirPath, filePath);
 
-program
-  .command('add')
-  .description('Add a new task')
-  .requiredOption('-n, --name <string>', 'Task name')
-  .requiredOption('-d, --deadline <string>', 'Task deadline (YYYY-MM-DD)')
-  .option('-t, --tags <tags...>', 'List of tags')
-  .action(createTask);
+  program
+    .name('todo-cli')
+    .description('Manage your most important tasks from the comfort of your terminal')
+    .version('1.0.0');
 
-program.parse(process.argv);
+  program
+    .command('add')
+    .description('Add a new task')
+    .requiredOption('-n, --name <string>', 'Task name')
+    .requiredOption('-d, --deadline <string>', 'Task deadline (YYYY-MM-DD)')
+    .option('-t, --tags <tags...>', 'List of tags')
+    .action(createTask);
+
+  program.parse(process.argv);
+};
+
+run();
