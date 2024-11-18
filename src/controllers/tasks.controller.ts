ed81b670c6177618c9f82586bd18762bacba2390
noxiousghost <sapkota.king@gmail.com>
2024-11-18 14:47:17 +0545

    add feature for viewing all and archived tasks

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Task, TaskSchema, TaskStatus } from '@src/model/zod.schema';
 import { v4 as uuidv4 } from 'uuid';
 import { writeJsonFile, readJsonFile } from '@src/utils/jsonOperations';
+import { error } from 'console';
 
 export const createTask = async (options: Omit<Task, 'id' | 'status' | 'isArchived'>): Promise<void> => {
   try {
@@ -18,8 +19,8 @@ export const createTask = async (options: Omit<Task, 'id' | 'status' | 'isArchiv
       console.error('Validation failed:', parsedTask.error.flatten());
       return;
     }
-    const readJsonRes = await readJsonFile();
-    tasks = readJsonRes;
+    tasks = await readJsonFile();
+    // tasks = readJsonRes;
     tasks.push(newTask);
 
     await writeJsonFile(tasks);
@@ -27,5 +28,36 @@ export const createTask = async (options: Omit<Task, 'id' | 'status' | 'isArchiv
     console.log(newTask);
   } catch (error) {
     console.error(error);
+  }
+};
+
+export const viewTasks = async (options: { all: string; archived: string }): Promise<void> => {
+  try {
+    if (Object.keys(options).length === 0) {
+      throw error('No options provided');
+    }
+    let tasks = [];
+    tasks = await readJsonFile();
+    // checking if task is archived or not
+    if (options.archived) {
+      tasks = tasks.filter((task) => task.isArchived);
+    } else if (options.all) {
+      tasks = tasks.filter((task) => !task.isArchived);
+    }
+
+    // displaying all the tasks result
+    if (tasks.length > 0) {
+      console.log('--------------------------------------------------------------');
+      tasks.forEach((task) => {
+        console.log(
+          `- ${task.name} (Status: ${task.status}, Deadline: ${task.deadline}, Tags: ${task.tags.join(', ')})\n`,
+        );
+      });
+      console.log('--------------------------------------------------------------');
+    } else {
+      console.log('No tasks found!!');
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
