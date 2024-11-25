75fe53776353b4faa498f4f90cf22604af357d57
noxiousghost <sapkota.king@gmail.com>
2024-11-25 11:05:50 +0545

    add docs to give overview of each and every methods.

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -3,6 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 import { writeJsonFile, readJsonFile } from '@src/utils/jsonOperations';
 import Logger from '@src/utils/displayOutputs';
 
+/** Function to create a new Task after taking options from the CLI. Accepted params are name, deadline and tags and they are required.
+ * It also checks and validate the new task before checking by the help of zod safeParse method.
+ *
+ * @param options name:string, deadline:date and tags:string[]
+ */
 export const createTask = async (options: Omit<Task, 'id' | 'status' | 'isArchived'>): Promise<void> => {
   try {
     const newTask: Task = {
@@ -31,6 +36,12 @@ export const createTask = async (options: Omit<Task, 'id' | 'status' | 'isArchiv
   }
 };
 
+/** Function that takes an type FilterTask and used to view and filter tasks based on the CLI command. It is in early return pattern checking
+ * for valid filtering options first and if no options are passed then all tasks are displayed.
+ * Refer to the project docs to see the valid options.
+ *
+ * @param options FilterTask
+ */
 export const viewTasks = async (options: FilterTask): Promise<void> => {
   try {
     const tasks = await readJsonFile();
@@ -72,6 +83,12 @@ export const viewTasks = async (options: FilterTask): Promise<void> => {
   }
 };
 
+/** Function that takes an object options with id and complete and used delete the task(s) from the list.
+ * id is the valid id of a task and complete means all the tasks that are with complete status.
+ * Refer to the project docs for more details about this feature.
+ *
+ * @param options id:string complete:string
+ */
 export const deleteTask = async (options: { id: string; complete: string }): Promise<void> => {
   try {
     let tasksAfterDelete: Task[] = [];
@@ -100,6 +117,12 @@ export const deleteTask = async (options: { id: string; complete: string }): Pro
   }
 };
 
+/** Function that takes id and options object with status and archive properties and used to modify the task details.
+ * Currently, it is possible to edit the status and archive status of task.
+ *
+ * @param id actual task id:string
+ * @param options status:string archive: string
+ */
 export const modifyTask = async (id: string, options: { status: string; archive: string }): Promise<void> => {
   try {
     const tasks = await readJsonFile();
@@ -134,6 +157,11 @@ export const modifyTask = async (id: string, options: { status: string; archive:
   }
 };
 
+/** Function that takes title of type string to search for tasks in the list. It uses regex.test method to display all the tasks that pass the
+ * regex pattern as specified by the user.
+ *
+ * @param title string
+ */
 export const searchTask = async (title: string): Promise<void> => {
   try {
     const tasks = await readJsonFile();
